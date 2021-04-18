f0bc8c832e314bf1ab40c960cc85c6d620f8d4ee
Mikael M <mikael_96@live.com>
2021-04-18 20:44:54 +0300

    ensuring unique work with replace policy

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -19,10 +19,7 @@ package com.example.background
 import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.background.workers.BlurWorker
 import com.example.background.workers.CleanupWorker
 import com.example.background.workers.SaveImageToFileWorker
@@ -49,7 +46,10 @@ class BlurViewModel(application: Application) : AndroidViewModel(application) {
      */
     internal fun applyBlur(blurLevel: Int) {
         var continuation = workManager
-            .beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java))
 
         // Add WorkRequests to blur the image the number of times requested
         for (i in 0 until blurLevel) {
